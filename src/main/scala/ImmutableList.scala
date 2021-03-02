88b6c0440594d6084fa9e983a26561fc303a3838
Chaya Mushky Shainer <mushky@innovid.com>
2021-03-02 12:10:57 +0200

    build classes and constructor for immutable list

--- src/main/scala/ImmutableList.scala
+++ src/main/scala/ImmutableList.scala
@@ -1,7 +1,31 @@
-class ImmutableList[T] {
+class ImmutableList[A] extends TImmutableList[A] {
+  private var first:Node[A] = null
+  private var last:Node[A] = null
+  //val add:Node[A]
 
-  def addItem(a:T) = {
+  def this(values:A*) = {
+    this()
+    if(values.size>=1) {
+      var current = null: Node[A]
+      var previous = null: Node[A]
+
+      for(v <- values) {
+        current = new Node[A](v, previous)
+        if (first == null) first = current
+        else previous.ChangeNext(current)
+        previous = current
+      }
+      last = current
+    }
+
+
 
   }
 
+  override def add(item: A): TImmutableList[A] = {
+
+    new ImmutableList[A] ()
+  }
 }
+
+
